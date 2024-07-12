c493fc062ba299b0673397d910e5d56ea35eff41
Shruti Mishra <imshruti470@gmail.com>
2024-07-12 13:41:59 +0530

    added new features

--- src/App.js
+++ src/App.js
@@ -2,16 +2,17 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Weather from './weather';
 import './app.css'
+import './api.json'
 
 function App() {
   const [weatherData, setWeatherData] = useState(null);
   const [city, setCity] = useState('London');
-  const API_KEY = 'ba71e9a3742f72ac0988091a2da97994';
+  const apiKey = process.env.REACT_APP_WEATHER_API_KEY;
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`);
+        const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`);
         setWeatherData(response.data);
       } catch (error) {
         console.error("Error fetching the weather data", error);
